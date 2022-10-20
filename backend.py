cc02f92bcba333ac441a7d47e88c677a5f6d7d0a
Chris Kemp <timkemp2023@u.northwestern.edu>
2022-10-20 11:17:26 -0500

    linking files

--- backend.py
+++ backend.py
@@ -1,13 +1,14 @@
 import flask
 from keyword_extract import *
-from Bing_API.py import *
-
+from Bing_API import *
+from keyword_extract import *
 app = flask.Flask(__name__)
 
 @app.route('/<string:paragraph>', methods=['GET'])
 def get_results(paragraph):
-    # DO KEYWORD EXTRACTION AND CALL SEARCH API
-    return paragraph
+    keyword = extract_ent(paragraph)[0]
+    results = bing_api(keyword)
+    return results
 
 
 if __name__ == '__main__':
