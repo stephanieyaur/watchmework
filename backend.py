9a70c9624da7a37d8dce02e109374eca4194f29f
Alex <alexttai44@gmail.com>
2022-10-21 18:41:49 -0500

    integrated parser into backend

--- backend.py
+++ backend.py
@@ -2,13 +2,15 @@ import flask
 from keyword_extract import *
 from Bing_API import *
 from keyword_extract import *
+from parse_json import *
 app = flask.Flask(__name__)
 
 @app.route('/<string:paragraph>', methods=['GET'])
 def get_results(paragraph):
     keyword = extract_ent(paragraph)[0]
     results = bing_api(keyword)
-    return results
+    p_results = parse_js(results)
+    return p_results
 
 
 if __name__ == '__main__':
