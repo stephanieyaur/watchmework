3b07d8608861f1c4f057e9d9124fd6b2118f17ea
Alex <alexttai44@gmail.com>
2022-10-26 18:34:09 -0500

    accounted for nonsense search terms

--- parse_json.py
+++ parse_json.py
@@ -1,34 +1,44 @@
 import json
 
-def _parsewebPages(pages):
+def _parsewebPages(dat):
     ret = []
-    for p in pages:
-        res = {
-            "title": p["name"],
-            "link": p["url"],
-            "preview": p["snippet"]
-        }
-        ret.append(res)
+    if "webPages" in dat:
+        for p in dat["webPages"]["value"]:
+            res = {
+                "title": p["name"],
+                "link": p["url"],
+                "preview": p["snippet"]
+            }
+            ret.append(res)
     return ret
 
 def _linktosearch(link):
     return link.split("=")[1].replace("+", " ").replace("%22", "\"")
+
+def _stringtolink(string):
+    return "https://www.bing.com/search?q="+string.replace(" ", "+").replace("\"", "%22")
+
+def _getsummary(dat):
+    if "entities" in dat:
+        return dat["entities"]["value"][0]["description"]
+    else:
+        return ""
 
 def parse_js(dat):
     #f = open(file)
     #dat = json.load(f)
     #f.close()
     ret = {
-        "searchlink": dat["webPages"]["webSearchUrl"],
-        "searchstring": _linktosearch(dat["webPages"]["webSearchUrl"]),
-        "summary": dat["entities"]["value"][0]["description"],
-        "results": _parsewebPages(dat["webPages"]["value"])
+        "searchstring": dat["queryContext"]["originalQuery"],
+        "searchlink": _stringtolink(dat["queryContext"]["originalQuery"]),
+        "summary": _getsummary(dat),
+        "results": _parsewebPages(dat),
     }
     return json.dumps(ret, indent=4)
     #return (dat["entities"]["value"][0]["description"], dat["webPages"]["value"][0]["displayUrl"])
 
 if __name__ == '__main__':
-    f = open('results/formatted_results.json')
+    f = open('results/nonsense.json')
     dat = json.load(f)
     f.close()
     parsed_res = parse_js(dat)
