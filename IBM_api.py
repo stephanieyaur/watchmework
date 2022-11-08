3777808f4e4f7512c188e5c901a6a0a8a63166a4
Chris Kemp <timkemp2023@u.northwestern.edu>
2022-11-08 11:19:12 -0600

    singe function to get topics, entities

--- IBM_api.py
+++ IBM_api.py
@@ -8,6 +8,12 @@ import Features, CategoriesOptions, EntitiesOptions
 api_key = 'mTAM4m9Xd1tUGg-iFXxlIVq1GKX0JbMkM4MUZVHL0trs'
 service_url = 'https://api.us-east.natural-language-understanding.watson.cloud.ibm.com/instances/0e13ef0f-3489-4f56-b11e-4d2b14b54c91'
 
+def get_analysis(document, paragraph):
+    topics = get_topics(document)
+    entities = get_entities(paragraph)
+
+    return topics['categories'], entities['entities']
+
 def get_topics(document):
     authenticator = IAMAuthenticator(api_key)
     natural_language_understanding = NaturalLanguageUnderstandingV1(
@@ -43,10 +49,7 @@ def get_entities(paragraph):
 
 if __name__ == '__main__':
     paragraph = 'Chicago, on Lake Michigan in Illinois, is among the largest cities in the United States. Known for its bold architecture, it has a skyline punctuated by skyscrapers such as the iconic John Hancock Center, 1,451-ft. Willis Tower (formerly the Sears Tower) and the neo-Gothic Tribune Tower. The city is also renowned for its museums, including the Art Institute of Chicago with its noted Impressionist and Post-Impressionist works. Lori Elaine Lightfoot is an American attorney and politician serving since 2019 as the 56th mayor of Chicago. She is a member of the Democratic Party. Before becoming mayor, Lightfoot worked in private legal practice as a partner at Mayer Brown and held various government positions in Chicago. The Bulls are the basketball located in Chicago. Players on the team include Demar DeRozen, and the team made it to the first round of the playoffs last year.'
-    topics = get_topics(paragraph)
-    entities = get_entities(paragraph)
-    print("ENTITIES:")
-    print(entities['entities'])
-    print("CATEGORIES:")
-    print(topics["categories"])
+    topics, entities = get_analysis(paragraph, paragraph)
+    print("TOPICS: ", topics)
+    print("ENTITIES: ", entities)
 
